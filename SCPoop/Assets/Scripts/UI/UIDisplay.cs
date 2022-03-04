6c11daa3deacb9dfb8236c65f6babf47d2d40063
Simon <simongonand@gmail.com>
2022-03-04 15:43:27 +0100

    add Ui

--- SCPoop/Assets/Scripts/UI/UIDisplay.cs
+++ SCPoop/Assets/Scripts/UI/UIDisplay.cs
@@ -11,8 +11,6 @@ public class UIDisplay : MonoBehaviour
     public TextMeshProUGUI txtPlayersPower;
     public TextMeshProUGUI txtEnemyPower;
 
-    public LifeSystem lifeSystem;
-
     private float currentPlayersHP;
     private float currentEnemyHP;
     private float playersPower;
@@ -37,5 +35,10 @@ public class UIDisplay : MonoBehaviour
         currentEnemyHP = LifeSystem.instance.enemylife;
         playersPower = Grid.instance.playerResult;
         enemyPower = Grid.instance.enemyResult;
+
+        txtPlayersHP.text = "Life = " + currentPlayersHP;
+        txtEnemyHP.text = "Enemy Life = " + currentEnemyHP;
+        txtPlayersPower.text = "Combined Power = " + playersPower;
+        txtEnemyPower.text = "Enemy Power = " + enemyPower;
     }
 }
