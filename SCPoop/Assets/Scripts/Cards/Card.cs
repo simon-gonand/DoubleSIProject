eaf20fc0f57cffe2c039b8024b2a678b86003988
Simon <simongonand@gmail.com>
2022-03-10 10:06:33 +0100

    fix arrows on first card

--- SCPoop/Assets/Scripts/Cards/Card.cs
+++ SCPoop/Assets/Scripts/Cards/Card.cs
@@ -26,7 +26,7 @@ public class Card : MonoBehaviour
     private MaterialPropertyBlock propBlock;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         if (stats != null)
             Init();
